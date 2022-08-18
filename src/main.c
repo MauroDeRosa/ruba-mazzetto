42b995cae5be591a81a5cab9e45d4c09bdb8db38
MauroDeRosa <it.mauro.derosa@gmail.com>
2022-08-18 17:26:12 +0200

    main file

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <io/log.h>
+#include <game/user.h>
+#include <game/history.h>
+#include <game/statistics.h>
+#include <network/server.h>
+#include <network/parser.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    srand(time(0));
+    
+    users_init();
+    history_init();
+    statistics_init();
+    game_saves_init();
+
+    system("sh ../frontend/launch.sh &");
+
+    server_init();
+    
+    while (!server_quit)
+    {
+        server_receive();
+        parse_request(client_message);
+    }
+
+    server_close();
+    return 0;
+}
